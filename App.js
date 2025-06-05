d6f593c11f51f0295df4a15ae2dc5b307e0932f6
robsonsena700 <robval.fsena@gmail.com.com>
2025-06-05 20:30:28 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,3 @@
+import 'primereact/resources/themes/lara-light-blue/theme.css'; // Ou outro tema disponível
+import 'primereact/resources/primereact.min.css';               
+import 'primeicons/primeicons.css';                            
