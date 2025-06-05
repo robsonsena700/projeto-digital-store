bb742f9930c367c4ff202556a59d2312a53f29a6
robsonsena700 <robval.fsena@gmail.com.com>
2025-06-05 20:43:41 -0300

    install react-router-dom

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
 import 'primereact/resources/themes/lara-light-blue/theme.css'; // Ou outro tema disponível
 import 'primereact/resources/primereact.min.css';               
 import 'primeicons/primeicons.css';                            
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default App;
